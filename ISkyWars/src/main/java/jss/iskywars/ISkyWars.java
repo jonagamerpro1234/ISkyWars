dd678a98e43b398f3e598b81f143a953184372b2
jonagamerpro1234 <js398704@gmail.com>
2022-02-12 01:25:59 -0300

    first commit

---
+++ ISkyWars/src/main/java/jss/iskywars/ISkyWars.java
@@ -0,0 +1,15 @@
+package jss.iskywars;
+
+import jss.iskywars.utils.ISkyWarsPlugin;
+
+public class ISkyWars extends ISkyWarsPlugin{
+
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
---
+++ ISkyWars/src/main/java/jss/iskywars/utils/ISkyWarsPlugin.java
@@ -0,0 +1,7 @@
+package jss.iskywars.utils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ISkyWarsPlugin extends JavaPlugin{
+
+}
